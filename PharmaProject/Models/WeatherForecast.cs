c796a2885172fd17e17942a5c5728de2b52c4bb2
akhila <akhila@AKHILA>
2025-05-27 11:46:25 -0400

    Add project files.

---
+++ PharmaProject/Models/WeatherForecast.cs
@@ -0,0 +1,15 @@
+namespace PharmaProject.Models
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+
+        public String Akhila { get; set; }
+    }
+}
